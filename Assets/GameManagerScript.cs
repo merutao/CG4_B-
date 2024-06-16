ca22f05ad6e6699aacd78b1fd1fb4024430596ef
ruka <115062996+merutao@users.noreply.github.com>
2024-06-16 20:36:14 +0900

    評価課題１

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
+using TMPro;
 
 public class GameManagerScript : MonoBehaviour
 {
     public GameObject block;
     public GameObject Goal;
+    public GameObject coin;
+    public GameObject goalParticle;
+
+    public TextMeshProUGUI scoreText;
+    public static int score = 0;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        score = 0;
+
         Screen.SetResolution(1920, 1080, false);
 
         Vector3 position = Vector3.zero;
@@ -43,10 +52,20 @@ public class GameManagerScript : MonoBehaviour
                 {
                     Instantiate(block, position, Quaternion.identity);
                 }
+
+                //GoalˆÊ’u
                 if (blockmap[y, x] == 2)
                 {
                     Instantiate(Goal, position, Quaternion.identity);
+                    Goal.transform.position = position;
+                    goalParticle.transform.position = position;
                 }
+                
+                if (blockmap[y, x] == 3)
+                {
+                    Instantiate(coin, position, Quaternion.identity);
+                }
+                
             }
             position.x = x;
         }
@@ -63,5 +82,7 @@ public class GameManagerScript : MonoBehaviour
                 SceneManager.LoadScene("TitleScene");
             }
        }
+
+        scoreText.text = "SCORE " + score;
     }
 }
--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -6,41 +6,58 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     public Rigidbody rb;
+    public GameObject bombParticle;
+
     private float moveSpeed;
 
     float distance = 0.6f;
 
     private bool isBlock = true;
 
+    private AudioSource audioSource;
+
+    private Animator animator;
+
+  
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = gameObject.GetComponent<AudioSource>();
+        transform.rotation = Quaternion.Euler(0, 90, 0);
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float stick = Input.GetAxis("Horizontal");
+
         if (GoalScript.isGameClear == false)
         {
             Vector3 v = rb.velocity;
             moveSpeed = 9;
-            if (Input.GetKey(KeyCode.RightArrow))
+            if (Input.GetKey(KeyCode.RightArrow)||stick > 0)
             {
                 v.x = moveSpeed;
+                transform.rotation = Quaternion.Euler(0, 90, 0);
+                animator.SetTrigger("walk");
             }
-            else if (Input.GetKey(KeyCode.LeftArrow))
+            else if (Input.GetKey(KeyCode.LeftArrow)||stick < 0)
             {
+                transform.rotation = Quaternion.Euler(0, -90, 0);
                 v.x = -moveSpeed;
+                animator.SetTrigger("walk");
             }
             else
             {
                 v.x = 0;
             }
 
-            if (Input.GetKeyDown(KeyCode.Space) && isBlock == true)
+            if (Input.GetButtonDown("Jump") && isBlock == true)
             {
                 v.y = moveSpeed;
+                animator.SetTrigger("Jump");
             }
             rb.velocity = v;
         }
@@ -62,4 +79,21 @@ public class PlayerScript : MonoBehaviour
             Debug.DrawRay(rayPosition,Vector3.down*distance, Color.yellow);
         }
     }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.gameObject.tag == "COIN")
+        {
+            other.gameObject.SetActive(false);
+            audioSource.Play();
+            GameManagerScript.score += 1;
+            Instantiate(bombParticle, transform.position, Quaternion.identity);
+        }
+        
+    }
+
+    
+
+    
+
 }
